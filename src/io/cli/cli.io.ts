79cad67f36f47021e70fdcbba0ca2d6c5ce581c1
max-im <pogidaevmo@gmail.com>
2022-12-02 00:14:55 +0200

    implement getting cli data

--- src/io/cli/cli.io.ts
+++ src/io/cli/cli.io.ts
@@ -2,23 +2,34 @@
 import { dataType, IInputData, IIo, sorterStrategy } from "../io.type";
 import { CliService } from './cli.service';
 
+
+const dataVariants: {[key: string]: dataType} = {
+    'Collection of Numbers': 'numbersCollection', 
+    'Text': 'string',
+    'Linked List': 'linkedList'
+};
+const sorterVariants: {[key: string]: sorterStrategy} = {
+    'Bubble Sort': 'bubbleSorter',
+    'Native JS Sort': 'nativeSorter',
+};
 export class CliIO implements IIo {
     private cliService: CliService = new CliService();
 
     // constructor() {}
 
     async input(): Promise<IInputData> {
-        const dataVariants: {[key: string]: dataType} = {
-            'Collection of Numbers': 'numbersCollection', 
-            'Text': 'string',
-            'Linked List': 'linkedList'
-        };
-        const sorterVariants: {[key: string]: sorterStrategy} = {'Bubble Sort': 'bubbleSort'};
-
         const dataType = await this.cliService.list<dataType>('Input Data Type', dataVariants);
         const sorterStrategy = await this.cliService.list<sorterStrategy>('Sort Strategy', sorterVariants);
 
-        return { dataType, sorterStrategy };
+        const secondSortData: {[key: string]: sorterStrategy} = {};
+        for (const key in sorterVariants) {
+            if (sorterVariants[key] !== sorterStrategy) {
+                secondSortData[key] = sorterVariants[key];
+            }
+        }
+        const secondSorterStrategy = await this.cliService.list<sorterStrategy>('Compare with Sort Strategy', secondSortData);
+
+        return { dataType, sorterStrategy, secondSorterStrategy };
     }
     output(): void {
         throw new Error("Method not implemented.");
--- src/io/io.type.ts
+++ src/io/io.type.ts
@@ -1,10 +1,11 @@
 export type dataType = 'string' | 'numbersCollection' | 'linkedList';
 
-export type sorterStrategy = 'bubbleSort';
+export type sorterStrategy = 'bubbleSorter' | 'nativeSorter';
 
 export interface IInputData {
     dataType: dataType
     sorterStrategy: sorterStrategy
+    secondSorterStrategy: sorterStrategy
 }
 
 export interface IIo {
